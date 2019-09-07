a2ff52fa22677fcbf1e467ae81c199850aa9bf53
Geoff Ramler <geoff@ramler.net>
2019-09-07 01:13:47 -0700

    Write useQuoteForm custom hook

--- src/hooks.js
+++ src/hooks.js
@@ -1,7 +1,25 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 const useQuoteForm = (callback) => {
-      return null;
+  const [inputs, setInputs] = useState({});
+
+  const handleSubmit = (event) => {
+    if (event) {
+      event.preventDefault();
+    }
+    callback();
+  }
+
+  const handleInputChange = (event) => {
+      event.persist();
+      setInputs(inputs => ({...inputs, [event.target.name]: event.target.value}));
+  }
+  
+  return(
+    handleSubmit,
+    handleInputChange,
+    inputs
+  );
 }
 
 export default useQuoteForm;

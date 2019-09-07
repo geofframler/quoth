e8b7dbdf08ca02dfb6861bfb2d75e12e557e28aa
Geoff Ramler <geoff@ramler.net>
2019-09-07 00:55:20 -0700

    Create hooks.js, create useQuoteForm custom hook

--- src/CreateQuote.js
+++ src/CreateQuote.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useQuoteForm } from './hooks';
 import './createquote.css';
 
 function CreateQuote() {
---
+++ src/hooks.js
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+
+const useQuoteForm = (callback) => {
+      return null;
+}
+
+export default useQuoteForm;

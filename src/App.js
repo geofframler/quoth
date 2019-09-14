68c5029ffbca1aad19d3c61ff6821e00a61901bf
Geoff Ramler <geoff@ramler.net>
2019-09-14 15:30:18 -0700

    Allow SortBar to manage its own states

--- src/App.js
+++ src/App.js
@@ -9,19 +9,15 @@ import EditModal from './components/EditModal';
 function App() {
   const [quotes, setQuotes] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [sort, setSort] = useState('id&_order=desc');
   const [page, setPage] = useState('1');
   const [perPage, setPerPage] = useState('20');
-  const [search, setSearch] = useState('');
-  const [searchResult, setSearchResult] = useState('');
+  
 
   // Fetch quotes from server
-  const getQuotes = (event) => {
-    if (event) event.preventDefault();
-    return fetch('http://localhost:3001/quotes?_sort=' + sort + '&q=' + search)
+  const getQuotes = () => {
+    return fetch('http://localhost:3001/quotes?_sort=id&_order=desc')
       .then(res => res.json())
       .then(res => setQuotes(res))
-      .then(setSearchResult(search))
       .then(setLoading(false));
   }
 
@@ -37,18 +33,14 @@ function App() {
       <InputForm getQuotes={getQuotes} />
 
       <SortBar
-        getQuotes={getQuotes}
-        setSort={setSort}
+        quotes={quotes}
+        setQuotes={setQuotes}
+        setLoading={setLoading}
         perPage={perPage}
-        setPerPage={setPerPage}
-        search={search}
-        setSearch={setSearch}
-        searchResult={searchResult} />
+        setPerPage={setPerPage} />
 
       {loading ? <div id="loading"><h4>Loading Quotes</h4><span uk-spinner="ratio: 4.5"></span></div> :
         <React.Fragment>
-          {quotes.length > 0 && searchResult && <div id="search-results">Search: "{searchResult}"</div>}
-          {quotes.length === 0 && searchResult && <div id="search-results"><br />There are no results containing "{searchResult}".<br />You can change that! Add a quote above.</div>}
           <div id="quote-list" uk-scrollspy="target: > div; cls: uk-animation-slide-top-small; delay: 20">
             {quotes.map((quote) => {
               return (
--- src/components/SortBar.js
+++ src/components/SortBar.js
@@ -1,45 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/sortbar.css';
 
 const SortBar = (props) => {
+  const [sort, setSort] = useState('id&_order=desc');
+  const [search, setSearch] = useState('');
+  const [searchResult, setSearchResult] = useState('');
+
+  const searchQuotes = (event) => {
+    if (event) event.preventDefault();
+    return fetch('http://localhost:3001/quotes?_sort=' + sort + '&q=' + search)
+      .then(props.setLoading(true))
+      .then(res => res.json())
+      .then(res => props.setQuotes(res))
+      .then(setSearchResult(search))
+      .then(props.setLoading(false));
+  }
+
   return (
-    <div uk-sticky="sel-target: .uk-navbar-container; cls-active: uk-navbar-sticky; bottom: #transparent-sticky-navbar">
-      <nav className="uk-navbar-container">
-        <div className="sort-bar uk-flex">
+    <React.Fragment>
+      <div uk-sticky="sel-target: .uk-navbar-container; cls-active: uk-navbar-sticky; bottom: #transparent-sticky-navbar">
+        <nav className="uk-navbar-container">
+          <div className="sort-bar uk-flex">
 
-          <div className="uk-navbar-left">
-            <form className="uk-flex-inline" onSubmit={props.getQuotes}>
-              <select className="uk-select" onChange={(event) => props.setSort(event.target.value)}>
-                <option value={'id&_order=desc'}>New to Old</option>
-                <option value={'id&_order=asc'}>Old to New</option>
-                <option value={'author&_order=asc'}>Author A-Z</option>
-                <option value={'author&_order=desc'}>Author Z-A</option>
-              </select>
-              <select className="uk-select" value={props.perPage} onChange={(event) => props.setPerPage(event.target.value)}>
-                <option value="10">10 per page</option>
-                <option value="20">20 per page</option>
-                <option value="50">50 per page</option>
-              </select>
-              <button type="submit" className="uk-button uk-button-text">Update</button>
-            </form>
-          </div>
-
-          <div className="uk-navbar-center">
-            <button id="js-scroll-trigger" className="uk-button uk-button-link" href="" uk-scroll="true" uk-totop="true" duration="500" />
-          </div>
-
-          <div className="uk-navbar-right">
-            <div className="uk-margin">
-              {props.searchResult && <a href="" type="submit" className="uk-button uk-button-text">clear search</a>}
-              <form className="uk-search uk-search-default" onSubmit={props.getQuotes}>
-                <button type="submit" href="" className="uk-button uk-button-link uk-search-icon-flip"><span className="search-button" uk-icon="icon: search; ratio: 1"></span></button>
-                <input className="uk-search-input" type="search" placeholder="Search..." onChange={(event) => props.setSearch(event.target.value)} />
+            <div className="uk-navbar-left">
+              <form className="uk-flex-inline" onSubmit={searchQuotes}>
+                <select className="uk-select" onChange={(event) => setSort(event.target.value)}>
+                  <option value={'id&_order=desc'}>New to Old</option>
+                  <option value={'id&_order=asc'}>Old to New</option>
+                  <option value={'author&_order=asc'}>Author A-Z</option>
+                  <option value={'author&_order=desc'}>Author Z-A</option>
+                </select>
+                <select className="uk-select" value={props.perPage} onChange={(event) => props.setPerPage(event.target.value)}>
+                  <option value="10">10 per page</option>
+                  <option value="20">20 per page</option>
+                  <option value="50">50 per page</option>
+                </select>
+                <button type="submit" className="uk-button uk-button-text">Update</button>
               </form>
             </div>
+
+            <div className="uk-navbar-center">
+              <button id="js-scroll-trigger" className="uk-button uk-button-link" href="" uk-scroll="true" uk-totop="true" duration="500" />
+            </div>
+
+            <div className="uk-navbar-right">
+              <div className="uk-margin">
+                {searchResult && <a href="" type="submit" className="uk-button uk-button-text">clear search</a>}
+                <form className="uk-search uk-search-default" onSubmit={searchQuotes}>
+                  <button type="submit" href="" className="uk-button uk-button-link uk-search-icon-flip"><span className="search-button" uk-icon="icon: search; ratio: 1"></span></button>
+                  <input className="uk-search-input" type="search" placeholder="Search..." onChange={(event) => setSearch(event.target.value)} />
+                </form>
+              </div>
+            </div>
           </div>
-        </div>
-      </nav>
-    </div>
+        </nav>
+      </div>
+      {props.quotes.length > 0 && searchResult && <div id="search-results">Search: "{searchResult}"</div>}
+      {props.quotes.length === 0 && searchResult && <div id="search-results"><br />There are no results containing "{searchResult}".<br />You can change that! Add a quote above.</div>}
+    </React.Fragment>
   );
 };
 
